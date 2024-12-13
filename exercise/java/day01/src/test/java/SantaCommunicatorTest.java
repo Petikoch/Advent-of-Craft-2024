8f5806a3b0b2b03eee928c099e83d935bc3965f1
Petikoch <petikoch@gmail.com>
2024-12-13 08:54:39 +0100

    Day 01: Santas Communication

--- exercise/java/day01/src/test/java/SantaCommunicatorTest.java
+++ exercise/java/day01/src/test/java/SantaCommunicatorTest.java
@@ -1,3 +1,5 @@
+import communication.Configuration;
+import communication.Reindeer;
 import communication.SantaCommunicator;
 import doubles.TestLogger;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,28 +11,27 @@ class SantaCommunicatorTest {
     private static final String DASHER = "Dasher";
     private static final String NORTH_POLE = "North Pole";
     private final int numberOfDaysToRest = 2;
-    private final int numberOfDayBeforeChristmas = 24;
+    private final int numberOfDaysBeforeChristmas = 24;
     private final TestLogger logger = new TestLogger();
     private SantaCommunicator communicator;
 
     @BeforeEach
     void setup() {
-        this.communicator = new SantaCommunicator(numberOfDaysToRest);
+        this.communicator = new SantaCommunicator(new Configuration(numberOfDaysToRest, numberOfDaysBeforeChristmas));
     }
 
     @Test
     void composeMessage() {
-        var message = communicator.composeMessage(DASHER, NORTH_POLE, 5, numberOfDayBeforeChristmas);
+        var message = communicator.composeMessage(
+                reindeer(5)
+        );
         assertThat(message).isEqualTo("Dear Dasher, please return from North Pole in 17 day(s) to be ready and rest before Christmas.");
     }
 
     @Test
     void shouldDetectOverdueReindeer() {
         var overdue = communicator.isOverdue(
-                DASHER,
-                NORTH_POLE,
-                numberOfDayBeforeChristmas,
-                numberOfDayBeforeChristmas,
+                reindeer(numberOfDaysBeforeChristmas),
                 logger);
 
         assertThat(overdue).isTrue();
@@ -42,11 +43,12 @@ class SantaCommunicatorTest {
     void shouldReturnFalseWhenNoOverdue() {
         assertThat(
                 communicator.isOverdue(
-                        DASHER,
-                        NORTH_POLE,
-                        numberOfDayBeforeChristmas - numberOfDaysToRest - 1,
-                        numberOfDayBeforeChristmas,
+                        reindeer(numberOfDaysBeforeChristmas - numberOfDaysToRest - 1),
                         logger)
         ).isFalse();
+    }
+
+    private static Reindeer reindeer(int numbersOfDaysForComingBack) {
+        return new Reindeer(DASHER, NORTH_POLE, numbersOfDaysForComingBack);
     }
 }
