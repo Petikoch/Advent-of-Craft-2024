98f09a9353f442e514c1b476fe16462b6c4273e0
Petikoch <petikoch@gmail.com>
2024-12-20 16:02:06 +0100

    Day 7: Read the tests and test your reading...

--- exercise/java/day07/src/test/java/WorkshopTest.java
+++ exercise/java/day07/src/test/java/WorkshopTest.java
@@ -3,6 +3,8 @@ import workshop.Gift;
 import workshop.Status;
 import workshop.Workshop;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class WorkshopTest {
@@ -13,17 +15,26 @@ class WorkshopTest {
         var workshop = new Workshop();
         workshop.addGift(new Gift(TOY_NAME));
 
-        var completedGift = workshop.completeGift(TOY_NAME);
+        var optionalGift = workshop.completeGift(TOY_NAME);
 
-        assertTrue(completedGift.isPresent());
-        assertEquals(Status.PRODUCED, completedGift.get().status());
+        assertGiftCompleted(optionalGift);
     }
 
     @Test
     void completingANonExistingGiftShouldReturnEmpty() {
         var workshop = new Workshop();
-        var completedGift = workshop.completeGift("NonExistingToy");
 
+        var optionalGift = workshop.completeGift("NonExistingToy");
+
+        assertNoGiftCompleted(optionalGift);
+    }
+
+    private static void assertGiftCompleted(Optional<Gift> completedGift) {
+        assertTrue(completedGift.isPresent());
+        assertEquals(Status.PRODUCED, completedGift.get().status());
+    }
+
+    private static void assertNoGiftCompleted(Optional<Gift> completedGift) {
         assertFalse(completedGift.isPresent());
     }
 }
