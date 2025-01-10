d9189011dbfab6a9ad2924c0bb1e30081cb6952b
Petikoch <petikoch@gmail.com>
2025-01-10 11:28:18 +0100

    Day 9: Improve tests readability

--- exercise/java/day09/src/test/java/gift/SantaServiceTests.java
+++ exercise/java/day09/src/test/java/gift/SantaServiceTests.java
@@ -1,6 +1,7 @@
 package gift;
 
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class SantaServiceTests {
@@ -9,19 +10,59 @@ class SantaServiceTests {
 
     @Test
     void requestIsApprovedForNiceChildWithFeasibleGift() {
-        Child niceChild = new Child("Alice", "Thomas", 9, Behavior.NICE, new GiftRequest("Bicycle", true, Priority.NICE_TO_HAVE));
+        Child niceChild = ChildBuilder
+                .aChild()
+                .nice()
+                .build();
         assertThat(service.evaluateRequest(niceChild)).isTrue();
     }
 
     @Test
     void requestIsDeniedForNaughtyChild() {
-        Child naughtyChild = new Child("Noa", "Thierry", 6, Behavior.NAUGHTY, new GiftRequest("SomeToy", true, Priority.DREAM));
+        Child naughtyChild = ChildBuilder
+                .aChild()
+                .naughty()
+                .build();
         assertThat(service.evaluateRequest(naughtyChild)).isFalse();
     }
 
     @Test
     void requestIsDeniedForNiceChildWithInfeasibleGift() {
-        Child niceChildWithInfeasibleGift = new Child("Charlie", "Joie", 3, Behavior.NICE, new GiftRequest("AnotherToy", false, Priority.DREAM));
+        Child niceChildWithInfeasibleGift =
+                ChildBuilder
+                        .aChild()
+                        .nice()
+                        .infeasibleGift()
+                        .build();
         assertThat(service.evaluateRequest(niceChildWithInfeasibleGift)).isFalse();
+    }
+
+    private static class ChildBuilder {
+
+        private Behavior behavior = null;
+        private boolean isFeasible = true;
+
+        public static ChildBuilder aChild() {
+            return new ChildBuilder();
+        }
+
+        public ChildBuilder nice() {
+            behavior = Behavior.NICE;
+            return this;
+        }
+
+        public ChildBuilder naughty() {
+            behavior = Behavior.NAUGHTY;
+            return this;
+        }
+
+        public ChildBuilder infeasibleGift() {
+            isFeasible = false;
+            return this;
+        }
+
+        public Child build() {
+            return new Child("AnyFirstName", "AnyLastName", 10, behavior, new GiftRequest("AnyToy", isFeasible, Priority.DREAM));
+        }
     }
 }
